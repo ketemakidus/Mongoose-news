32e49005019396e02f3850ede71b7372aa1708eb
Kidus <Ketemakidus009@gmail.com>
2019-03-30 14:19:11 -0400

    capital note

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/premireleaguedata", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/premireleaguedata", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/premireleaguedata", ;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 
