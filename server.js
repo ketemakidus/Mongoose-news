5e69253b75794159808cbc0090296a6cf3ee6d23
Kidus <Ketemakidus009@gmail.com>
2019-03-31 16:38:48 -0400

    deploye

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/premireleaguedata";
 
 
 var axios = require("axios");
@@ -8,29 +10,19 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3000;
 
 var app = express();
-
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/premireleaguedata", { useNewUrlParser: true });
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/premireleaguedata";
-
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
-
-
-
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.skysports.com/").then(function(response) {
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.skysports.com/").then(function (response) {
     var $ = cheerio.load(response.data);
 
-    $("h3").each(function(i, element) {
+    $("h3").each(function (i, element) {
       var result = {};
 
       result.title = $(this)
@@ -39,15 +31,15 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
-        // result.img = $(this)
-        // .find("img")
-        // .attr("src");
+      // result.img = $(this)
+      // .find("img")
+      // .attr("src");
 
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           console.log(err);
         });
     });
@@ -56,49 +48,49 @@ app.get("/scrape", function(req, res) {
   });
 });
 
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-app.get("/note", function(req, res){
+app.get("/note", function (req, res) {
   db.Article.find({})
-  .then(function(dbArticle){
-    res.json(dbArticle);
-  })
-  .catch(function(err){
-    res.json(err);
-  })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    })
 })
 
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   db.Article.findOne({ _id: req.params.id })
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
